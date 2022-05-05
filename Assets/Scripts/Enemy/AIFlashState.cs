7035ae5e934ddc1ae38c44b4f1afdebdf277f149
CPingel023 <CWPingel@student.fullsail.edu>
2022-05-05 15:06:38 -0400

    AIFlash and AIHitbox file commit

---
+++ Assets/Scripts/Enemy/AIFlashState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFlashState : AIState
+{
+    float flashTimer = 1.0f;
+    public AIStateID GetId()
+    {
+        return AIStateID.Flashed;
+    }
+    public void Enter(AIAgent agent)
+    { 
+        agent.mesh.material.color = Color.white;
+    }
+    public void Update(AIAgent agent)
+    {
+        flashTimer -= Time.deltaTime;
+        if (flashTimer == 0.0f)
+        {
+            agent.mesh.material.color = Color.red;
+            agent.stateMachine.ChangeState(AIStateID.Idle);
+        }
+    }
+    public void Exit(AIAgent agent)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Enemy/Hitbox.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    public Health health;
+
+    public void OnRaycastHit(BaseWeapon weapon, Vector3 direction)
+    {
+        //modify 25f with the bullet.damage after implementation.
+        health.TakeDamage(25f, direction);
+    }
+
+}
