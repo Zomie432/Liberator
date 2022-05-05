e5f0b49e3c938d6ca87b8591ebd51967df96b989
cjortiz2002 <cjortiz626@gmail.com>
2022-05-05 15:24:56 -0400

    Implemented Temporary Pause Button for testing and Implemented Resume functionality.

--- Assets/Scripts/HUD/ButtonFunctionality.cs
+++ Assets/Scripts/HUD/ButtonFunctionality.cs
@@ -4,28 +4,43 @@ using UnityEngine;
 
 public class ButtonFunctionality : MonoBehaviour
 {
+
+
+    GameObject pause;
+    GameObject mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pause = GameObject.FindGameObjectWithTag("PauseMenu");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        mainCamera.SetActive(false);
+        Debug.Log("Game Paused");
     }
     public void Resume()
     {
-
+        Time.timeScale = 1f;
+        mainCamera.SetActive(true);
+        pause.SetActive(false);
+        Debug.Log("Resuming Level");
     }
 
     public void Restart()
     {
-
+        Debug.Log("Restarting Level");
     }
 
-   public void Quit()
+    public void Quit()
     {
         Application.Quit();
         Debug.Log("Application is Exiting");
