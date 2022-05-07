976a776349c58514f9cc9e2726384afd78be0196
cjortiz2002 <cjortiz626@gmail.com>
2022-05-07 14:36:05 -0400

    Fixed Health Bar not updating when damage is taken & Implemented a color change in the health bar once below 25% max health

--- Assets/Scripts/HUD/HealthBar.cs
+++ Assets/Scripts/HUD/HealthBar.cs
@@ -7,17 +7,22 @@ public class HealthBar : MonoBehaviour
 {
 
     public Slider slider;
-
+    public Gradient gradient;
+    public Image fill;
     public void SetMaxHealth()
     {
         // Sets Slider max health to be whatever the players max health is
         slider.maxValue = GameManager.Instance.playerScript.GetPlayersMaxHealth();
-    }    
+
+        fill.color = gradient.Evaluate(1f);
+    }
 
     public void UpdateHealthBar()
     {
         // Updates Slider value to represent the players current health
         slider.value = GameManager.Instance.playerScript.GetCurrentPlayerHealth();
+        Debug.Log("Slider value = " + slider.value);
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
 
