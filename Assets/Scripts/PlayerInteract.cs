57fe851f4d7c5680fe4a17f6115fccc9d33e7527
Curtis Ohlmeier <CBOhlmeier@student.fullsail.edu>
2022-05-06 23:15:18 -0400

    added tag to main camera for Vrij_Player.prefab

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
-    
     public void ProcessInteraction()
     {
+        //raycast forward from the player's view
+
+        //check the tag of the game object it collides with if it does collide
+
+        //if door then access door controller script on that object and call door.Interact() which will decide whether to open or close
 
     }
 }
