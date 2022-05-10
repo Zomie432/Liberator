d46fd0c062fc616e17e88c43bd9cfb33fc19121f
Curtis Ohlmeier <CBOhlmeier@student.fullsail.edu>
2022-05-09 23:25:34 -0400

    Created new door open and close animations called DoorOpenNew.anim and DoorCloseNew.anim

--- Assets/Scripts/Door/DoorController.cs
+++ Assets/Scripts/Door/DoorController.cs
@@ -16,7 +16,7 @@ public class DoorController : MonoBehaviour
 
     public void Interact()
     {
-        isPlaying = (doorAnimator.GetCurrentAnimatorStateInfo(0).IsName("DoorOpen") || doorAnimator.GetCurrentAnimatorStateInfo(0).IsName("DoorClose")) && doorAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f;
+        isPlaying = (doorAnimator.GetCurrentAnimatorStateInfo(0).IsName("DoorOpenNew") || doorAnimator.GetCurrentAnimatorStateInfo(0).IsName("DoorCloseNew")) && doorAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f;
 
         //check to see if an animation is already playing, if so we don't want to interact
         if(!isPlaying)
@@ -24,12 +24,12 @@ public class DoorController : MonoBehaviour
             //neither animation is currently playing so...
             if (doorOpen == false)
             {
-                doorAnimator.Play("DoorOpen", 0, 0.0f);
+                doorAnimator.Play("DoorOpenNew", 0, 0.0f);
                 doorOpen = true;
             }
             else if (doorOpen == true)
             {
-                doorAnimator.Play("DoorClose", 0, 0.0f);
+                doorAnimator.Play("DoorCloseNew", 0, 0.0f);
                 doorOpen = false;
             }
         }
